59ea7f53f1556e2537493d08caf0fc9fe7d82a91
Kelly Bondanza <kelly.bondanza@gmail.com>
2018-10-28 20:14:15 -0400

    first 2 modules

---
+++ sick-fits/frontend/components/Meta.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+import React from "react";
+
+const Meta = () => {
+  return (
+    <Head>
+      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+      <title>Sick Fits</title>
+    </Head>
+  );
+};
+
+export default Meta;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Sell = () => <h1>sell</h1>;
+
+export default Sell;
